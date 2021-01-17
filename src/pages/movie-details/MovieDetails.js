5973d29bef5b82163ec8caabe84f81a6f3501758
Ann <kharko2112.mechmat@gmail.com>
2021-01-17 11:52:39 +0200

    something plus

--- src/pages/movie-details/MovieDetails.js
+++ src/pages/movie-details/MovieDetails.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useParams, useRouteMatch} from 'react-router-dom';
 import {moviesService} from "../../services";
+import styles from './MovieDetails.module.css'
 
 export const MovieDetails = () => {
     // const {params: {id}} = useRouteMatch();
@@ -30,7 +31,11 @@ export const MovieDetails = () => {
     }
 
     return (
-        <div>
+        <div className={styles.details}>
+            <div className={styles.image}>
+                <img src={`https://image.tmdb.org/t/p/w300${filmDetails.poster_path}`} alt={`${filmDetails.original_title} poster`}/>
+
+            </div>
             <h1>{filmDetails.original_title}</h1>
             <h2>{filmDetails.genres.map(el => <span key={el.id}>{el.name} -</span>)}</h2>
             <h3>{filmDetails.tagline}</h3>
